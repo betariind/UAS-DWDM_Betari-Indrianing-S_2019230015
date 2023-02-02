65fca98d379fc755f68e6f7f43426fc8aadb3c9b
betariind <59275120+betariind@users.noreply.github.com>
2023-02-02 09:31:32 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import pickle
+import streamlit as st
+
+pickle_in = open('model_uas.pkl', 'rb')
+lr = pickle.load(pickle_in)
+
+def welcome():
+    return 'Hallo'
+
+def prediction(age, sex, bmi, children, smoker):
+
+    prediction = lr.predict([[age, sex, bmi, children, smoker]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Aplikasi Prediksi Besar Numerik Asuransi dengan Algoritma Regresi Linier")
+    st.markdown('creator: Betari Indrianing Sugiarto (2019230015)')
+    st.write('\n')
+    st.markdown('Lengkapi data berikut:')
+    
+    st.write('\n')
+    age = st.text_input("Age", "0")
+    sex = st.text_input("Sex", "0")
+    bmi = st.text_input("BMI", "0")
+    children = st.text_input("Children", "0")
+    smoker = st.text_input("Smoker", "0")
+    result =""
+    
+    if st.button("REVEAL PREDIKSI"):
+        result = prediction(age, sex, bmi, children, smoker)
+    st.success('Hasil Prediksi Numerik- {}'.format(result))
+    
+if __name__=='__main__':
+    main()
